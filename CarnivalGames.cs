8f7f789bb2b7df82f9c66b67b685024bf3192d5b
Jeff Erdman <jerdman81@gmail.com>
2022-01-05 17:13:15 -0500

    initial commit - basic pocos

---
+++ CarnivalGames.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarnivalConsoleGame
+{
+    public class CarnivalGames
+    {
+       public CarnivalGames(){}
+
+        public CarnivalGames(string name, int time, int tickets, int funPoints)
+        {
+            Name = name;
+            Time = time;
+            Tickets = tickets;
+            FunPoints = funPoints;
+        }
+        public string Name { get; set; }
+        public int Time { get; set; }
+        public int Tickets { get; set; }
+        public int FunPoints { get; set; }
+        // POCOS
+
+
+        //Ticket Booth
+
+        //MerryGoRound
+
+        //BaseballToss
+
+        //Coaster
+
+        //BasketToss
+
+        //FerrisWheel
+
+        //RingToss
+
+        //BalloonDart
+
+        //TiltAWhirl
+
+        //EntranceExit
+    }
+
+}
+    
+}
---
+++ ProgramUI.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarnivalConsoleGame
+{
+    public class ProgramUI
+    {
+        int tickets = 0;
+        int money = 50;
+        int time = 120;
+        int funScore = 0;
+
+        //Ticket Booth
+        //EntranceExit
+
+        //MerryGoRound
+        public static CarnivalGames merryGoRound = new CarnivalGames(
+           "Merry-Go-Round",  //name
+           -5,  //time
+           -3,  //tickets
+           2  //fun points
+           );
+
+        //BaseballToss
+        public static CarnivalGames baseballToss = new CarnivalGames(
+           "Baseball Toss",  //name
+           -5, //time
+           -3, //tickets
+           2  //fun points
+           );
+        //Coaster
+        public static CarnivalGames rollerCoaster = new CarnivalGames(
+            "Roller Coaster",  //name
+            -5, //time
+            -3, //tickets
+            2  //fun points
+            );
+        //BasketToss
+        public static CarnivalGames basketToss = new CarnivalGames(
+           "Basket Toss",  //name
+           -5, //time
+           -3, //tickets
+           2  //fun points
+           );
+        //FerrisWheel
+        public static CarnivalGames ferrisWheel = new CarnivalGames(
+           "Ferris Wheel",  //name
+           -5, //time
+           -3, //tickets
+           2  //fun points
+           );
+        //RingToss
+        public static CarnivalGames ringToss = new CarnivalGames(
+            "Ring Toss",  //name
+            -5, //time
+            -3, //tickets
+            2  //fun points
+            );
+        //BalloonDart
+        public static CarnivalGames balloonDart = new CarnivalGames(
+            "Balloon Dart",  //name
+            -5, //time
+            -3, //tickets
+            2  //fun points
+            );
+        //TiltAWhirl
+        public static CarnivalGames tiltAWhirl = new CarnivalGames(
+           "Tilt-A-Whirl",  //name
+           -5, //time
+           -3, //tickets
+           2  //fun points
+           );
+
+        
+
+
+    }
+}
